5ba520f66f2f3818ff0a6cae23514363897707d9
Matz Larsson <matzlarsson94@gmail.com>
2016-12-09 19:10:36 +0100

    Added base code for web interface

---
+++ web/index.js
@@ -0,0 +1,15 @@
+var app = require('express')();
+var http = require('http').Server(app);
+
+//Start server
+var port = (process.argv.length>2?parseInt(process.argv[2]):80);
+http.listen(port, function(){
+	console.log("Server started, listening on port "+port);
+});
+
+//Fix file redirections
+app.get('/', function(req, res, next){ res.sendFile(__dirname+'/index.html'); });
+app.use('/imgs', function (req, res, next) { res.sendFile(__dirname+req.originalUrl); });
+app.use('/pages', function (req, res, next) { res.sendFile(__dirname+req.originalUrl); });
+app.use('/scripts', function (req, res, next) { res.sendFile(__dirname+req.originalUrl); });
+app.use('/styles', function (req, res, next) { res.sendFile(__dirname+req.originalUrl); });
