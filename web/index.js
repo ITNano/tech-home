e2fa63a49d221f8e6d3f7363cf76b0a0a3306ea2
Matz Larsson <matzlarsson94@gmail.com>
2017-12-08 23:15:06 +0100

    Updated index file

--- web/index.js
+++ web/index.js
@@ -1,12 +1,17 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var server = require('./server').Server(http);
+var express = require('express'), app = express();
+var http = require('http'), https = require("https");
+var fs = require("fs");
 
-//Start server
-var port = (process.argv.length>2?parseInt(process.argv[2]):80);
-http.listen(port, function(){
-	console.log("Server started, listening on port "+port);
-});
+var server = require('server').Server(http);
+
+
+var options = {
+    key: fs.readFileSync('/etc/letsencrypt/live/matzmatz.se/privkey.pem', 'utf8'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/matzmatz.se/fullchain.pem', 'utf8')
+};
+
+
+app.use(express.static('public'));
 
 //Fix file redirections
 app.get('/', function(req, res, next){ res.sendFile(__dirname+'/index.html'); });
@@ -14,3 +19,13 @@ app.use('/imgs', function (req, res, next) { res.sendFile(__dirname+req.original
 app.use('/pages', function (req, res, next) { res.sendFile(__dirname+req.originalUrl); });
 app.use('/scripts', function (req, res, next) { res.sendFile(__dirname+req.originalUrl); });
 app.use('/styles', function (req, res, next) { res.sendFile(__dirname+req.originalUrl); });
+
+
+var port = (process.argv.length>2?parseInt(process.argv[2]):80);
+http.createServer(app).listen(port+1, function(){
+    console.log("Running HTTP on port "+(port+1));
+});
+
+https.createServer(options, app).listen(port, function(){
+    console.log("Running HTTPS on port "+port);
+});
